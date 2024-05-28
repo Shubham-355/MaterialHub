a1564a24f7a2b9ecb2874f10652bee517b73b1dc
Shubham-355 <smsuthar2520@gmail.com>
2024-05-28 23:03:19 +0530

    Error \Test

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/files/**/*',
+          dest: 'files'
+        }
+      ]
+    })
+  ],
 })
